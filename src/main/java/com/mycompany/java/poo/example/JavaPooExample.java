f5805118153f12759cc09638728db525d77ca624
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-10-09 22:36:36 -0600

    changed: more easy

--- src/main/java/com/mycompany/java/poo/example/JavaPooExample.java
+++ src/main/java/com/mycompany/java/poo/example/JavaPooExample.java
@@ -3,13 +3,11 @@
  */
 package com.mycompany.java.poo.example;
 
-import javax.swing.JOptionPane;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-
+import java.util.Scanner;
 
 /**
  *
@@ -19,72 +17,71 @@ public class JavaPooExample {
 
     public static void main(String[] args) {
 
-       
+        Scanner scanner = new Scanner(System.in);
+
+        List<Client> clients = new ArrayList<>();
         List<Invoice> invoices = new ArrayList<>();
-        Map<String, Client> clientsMap = new HashMap<>(); // Mapa para almacenar clientes existentes por su ID
 
         while (true) {
             try {
-                String ID = JOptionPane.showInputDialog("Ingrese el ID del cliente (deben ser 9 caracteres):");
+                System.out.print("Ingrese el ID del cliente (deben ser 9 caracteres): ");
+                String ID = scanner.nextLine();
 
-                // Verificar si el ID del cliente ya existe en el mapa
-                if (clientsMap.containsKey(ID)) {
-                    // Si el cliente existe, obtener la instancia del cliente
-                    Client existingClient = clientsMap.get(ID);
-                    
-                    String IDInvoice = JOptionPane.showInputDialog("Ingrese el ID de la factura:");
-                    String nameClient = existingClient.getName();
-                    String bikeCode = JOptionPane.showInputDialog("Ingrese el código de la bicicleta:");
-                    String distanceStr = JOptionPane.showInputDialog("Ingrese la distancia recorrida en kilómetros:");
+                Client client = null;
 
-                    // Crear una instancia de Invoice y validar los datos
-                    Invoice invoice = new Invoice(IDInvoice, existingClient, nameClient, bikeCode, distanceStr);
-                    invoices.add(invoice);
-                } else {
-                    // Si el cliente no existe, solicitar y crear una nueva instancia de cliente
-                    String name = JOptionPane.showInputDialog("Ingrese el nombre del cliente:");
-                    String address = JOptionPane.showInputDialog("Ingrese la dirección del cliente (máximo 15 caracteres):");
-                    String ageStr = JOptionPane.showInputDialog("Ingrese la edad del cliente (debe ser mayor o igual a 18):");
-
-                    // Crear una instancia de Client y validar los datos
-                    Client client = new Client(name, ID, address, Integer.parseInt(ageStr));
-
-                    clientsMap.put(ID, client); // Agregar el cliente al mapa
-
-                    String IDInvoice = JOptionPane.showInputDialog("Ingrese el ID de la factura:");
-                    String nameClient = client.getName();
-                    String bikeCode = JOptionPane.showInputDialog("Ingrese el código de la bicicleta:");
-                    String distanceStr = JOptionPane.showInputDialog("Ingrese la distancia recorrida en kilómetros:");
-
-                    // Crear una instancia de Invoice y validar los datos
-                    Invoice invoice = new Invoice(IDInvoice, client, nameClient, bikeCode, distanceStr);
-                    invoices.add(invoice);
+                // Buscar el cliente por su ID en la lista de clientes
+                for (Client c : clients) {
+                    if (c.getID().equals(ID)) {
+                        client = c;
+                        break;
+                    }
                 }
 
-                int continueOption = JOptionPane.showConfirmDialog(null, "¿Desea ingresar otra factura?", "Continuar", JOptionPane.YES_NO_OPTION);
-                if (continueOption == JOptionPane.NO_OPTION) {
+                if (client == null) {
+                    System.out.print("Ingrese el nombre del cliente: ");
+                    String name = scanner.nextLine();
+
+                    System.out.print("Ingrese la dirección del cliente (máximo 15 caracteres): ");
+                    String address = scanner.nextLine();
+
+                    System.out.print("Ingrese la edad del cliente (debe ser mayor o igual a 18): ");
+                    int age = Integer.parseInt(scanner.nextLine());
+
+                    client = new Client(name, ID, address, age);
+                    clients.add(client);
+                }
+
+                System.out.print("Ingrese el ID de la factura: ");
+                String IDInvoice = scanner.nextLine();
+                String nameClient = client.getName();
+
+                System.out.print("Ingrese el código de la bicicleta: ");
+                String bikeCode = scanner.nextLine();
+
+                System.out.print("Ingrese la distancia recorrida en kilómetros: ");
+                String distanceStr = scanner.nextLine();
+
+                Invoice invoice = new Invoice(IDInvoice, client, nameClient, bikeCode, distanceStr);
+                invoices.add(invoice);
+
+                System.out.print("¿Desea ingresar otra factura? (S/N): ");
+                String continueOption = scanner.nextLine().trim().toLowerCase();
+                if (!continueOption.equals("s")) {
                     break;
                 }
             } catch (IllegalArgumentException e) {
-                JOptionPane.showMessageDialog(null, "Error: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                System.out.println("Error: " + e.getMessage());
             }
         }
 
-        // Mostrar la información de todas las facturas ingresadas
-        StringBuilder invoiceInfo = new StringBuilder("Información de las facturas ingresadas:\n");
-        for (int i = 0; i < invoices.size(); i++) {
-            Invoice invoice = invoices.get(i);
-            invoiceInfo.append("Nombre del cliente: ").append(invoice.getNameClient()).append("\n");
-            invoiceInfo.append("ID del cliente: ").append(invoice.getIDClient()).append("\n");
-            invoiceInfo.append("Código de la bicicleta: ").append(invoice.getBikeCode()).append("\n");
-            invoiceInfo.append("Distancia recorrida (km): ").append(invoice.getDistance()).append("\n");
-            invoiceInfo.append("Monto a pagar: $").append(invoice.getAmount()).append("\n");
-            invoiceInfo.append("\n");
+        System.out.println("Información de las facturas ingresadas:");
+        for (Invoice invoice : invoices) {
+            System.out.println("Nombre del cliente: " + invoice.getNameClient());
+            System.out.println("ID del cliente: " + invoice.getIDClient());
+            System.out.println("Código de la bicicleta: " + invoice.getBikeCode());
+            System.out.println("Distancia recorrida (km): " + invoice.getDistance());
+            System.out.println("Monto a pagar: $" + invoice.getAmount());
+            System.out.println();
         }
-
-        JOptionPane.showMessageDialog(null, invoiceInfo.toString(), "Información de Facturas", JOptionPane.INFORMATION_MESSAGE);
     }
-
 }
-
-
